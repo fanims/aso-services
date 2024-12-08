c95d5543e0d44cbd8b350ab65139b7e0ecf84dfd
fanims <farhanathar49@gmail.com>
2024-12-08 19:44:30 +0500

    start banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     "*"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white': '#ffffff',
+        'black': '#000000',
+        'text-color': '#8492A7',
+        'text-light': '#D2EBFF',
+        'theme-color': '#008AFF',
+        'header-color': '#222D39',
+        'border-color': '#DEE7EE',
+        'background-color': '#F7F9FC',
+      },
+      fontFamily: {
+        'body-font': ["Hanken Grotesk", "sans-serif"],
+        'heading-font': ["Hanken Grotesk", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
